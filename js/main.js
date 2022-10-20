849b4f9366032d5e6c9ae406bd4191cab9153613
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-20 14:59:21 +0200

    Aggiunti prompt di richiesta all'utente

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//1 chiedi all'utente il numero dei km che vuole percorrere
+const kmToGo = Number(prompt("inserisci i km che vorrai percorrere"));
+
+//2 chiedi all'utente l'età del passeggero
+const age = Number(prompt("inserisci la tua età"));
+
